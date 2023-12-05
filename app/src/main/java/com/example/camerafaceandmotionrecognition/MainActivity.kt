e2272e1a0e97dbafdbdda1de37632ab84aab8776
EgorKholod <veh@kvartel.com>
2023-12-06 00:45:13 +0300

    add camera

--- app/src/main/java/com/example/camerafaceandmotionrecognition/MainActivity.kt
+++ app/src/main/java/com/example/camerafaceandmotionrecognition/MainActivity.kt
@@ -1,46 +1,181 @@
 package com.example.camerafaceandmotionrecognition
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ImageView.ScaleType
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.camera.core.CameraSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+
 import com.example.camerafaceandmotionrecognition.ui.theme.CameraFaceAndMotionRecognitionTheme
+import com.google.common.util.concurrent.ListenableFuture
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (permissionGranted()) {
+            initView()
+        } else {
+            requestPermission()
+        }
+
+    }
+
+    private fun permissionGranted() =
+        ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf( Manifest.permission.CAMERA), 0
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initView()
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun initView() {
         setContent {
-            CameraFaceAndMotionRecognitionTheme {
+            CameraFaceAndMotionRecognitionTheme  {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        var lens by remember { mutableStateOf(CameraSelector.LENS_FACING_FRONT) }
+                        CameraPreview(
+                            cameraLens = lens
+                        )
+                        Controls(
+                            onLensChange = { lens = switchLens(lens) }
+                        )
+                    }
                 }
             }
         }
     }
-}
+    @Composable
+    fun CameraPreview(
+        modifier: Modifier = Modifier,
+        cameraLens: Int,
+        scaleType: PreviewView.ScaleType = PreviewView.ScaleType.FILL_CENTER,
+    ) {
+        val lifecycleOwner = LocalLifecycleOwner.current
+        val context = LocalContext.current
+        val previewView = remember { PreviewView(context) }
+        val cameraProvider =
+            ProcessCameraProvider.getInstance(context)
+                .configureCamera(previewView, lifecycleOwner, cameraLens, context)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        AndroidView(
+            modifier = modifier,
+            factory = {
+                previewView.apply {
+                    this.scaleType = scaleType
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+                }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CameraFaceAndMotionRecognitionTheme {
-        Greeting("Android")
+                previewView
+            })
     }
+}
+
+
+@Composable
+fun Controls(
+    onLensChange: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 24.dp),
+        contentAlignment = Alignment.BottomCenter,
+    ) {
+        Button(
+            onClick = onLensChange,
+            modifier = Modifier.wrapContentSize()
+        ) { Icon(Icons.Filled.Refresh, contentDescription = "Switch camera") }
+    }
+}
+
+private fun ListenableFuture<ProcessCameraProvider>.configureCamera(
+    previewView: PreviewView,
+    lifecycleOwner: LifecycleOwner,
+    cameraSelector: CameraSelector,
+    context: Context
+): ListenableFuture<ProcessCameraProvider> {
+    addListener({
+        val preview = androidx.camera.core.Preview.Builder()
+            .build()
+            .apply {
+                setSurfaceProvider(previewView.surfaceProvider)
+            }
+
+        try {
+            get().apply {
+                unbindAll()
+                bindToLifecycle(
+                    lifecycleOwner, cameraSelector, preview
+                )
+            }
+        } catch (exc: Exception) {
+            TODO("process errors")
+        }
+    }, ContextCompat.getMainExecutor(context))
+    return this
+}
+
+private fun switchLens(lens: Int) = if (CameraSelector.LENS_FACING_FRONT == lens) {
+    CameraSelector.LENS_FACING_BACK
+} else {
+    CameraSelector.LENS_FACING_FRONT
 }
